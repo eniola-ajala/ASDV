d93d44cd9b5420961c2bbc1926cd2ef74f54ae6a
eniola-ajala <ajalaeniola454@gmail.com>
2025-11-11 18:45:30 +0000

    Add files via upload

--- ANOVA Tests/index.R
+++ ANOVA Tests/index.R
@@ -1,70 +1,135 @@
-# Installing packages
-install.packages("datarium")
-install.packages("RVAideMemoire")
-install.packages("car")
-install.packages("carData")
+# installing packages
+install.packages("TTR")
+install.packages("forecast")
+
+# calling the libraries
+library(TTR)
+library(forecast)
+
+kings <- scan("http://robjhyndman.com/tsdldata/misc/kings.dat",skip=3)
+kings
+
+# store the variable as time series object
+kingstimeseries <- ts(kings)
+kingstimeseries
+
+births <- scan("http://robjhyndman.com/tsdldata/data/nybirths.dat") 
+birthstimeseries <- ts(births, frequency = 12, start = c(1946, 1))
+birthstimeseries
+
+souvenir <- scan("http://robjhyndman.com/tsdldata/data/fancy.dat") 
+souvenirtimeseries <- ts(souvenir, frequency = 12, start = c(1987, 1))
+souvenirtimeseries
+
+# plotting time series
+plot.ts(kingstimeseries)
+plot.ts(birthstimeseries)
+plot.ts(souvenirtimeseries)
+
+# log-transform timeseries
+logsouvenirtimeseries <- log(souvenirtimeseries)
+plot.ts(logsouvenirtimeseries)
+
+# smoothing time series
+kingstimeseriesSMA3 <- SMA(kingstimeseries, n=3)
+plot.ts(kingstimeseriesSMA3)
+
+#smoothing with higher order
+kingstimeseriesSMA8 <- SMA(kingstimeseries, n=8)
+plot.ts(kingstimeseriesSMA8)
+
+birthstimeseriescomponents <- decompose(birthstimeseries)
+birthstimeseriescomponents$seasonal
+plot(birthstimeseriescomponents)
+
+# seasonally adjusting
+birthstimeseriescomponents <- decompose(birthstimeseries)
+birthstimeseriesseasonallyadjusted <- birthstimeseries - birthstimeseriescomponents$seasonal
+
+plot(birthstimeseriesseasonallyadjusted)
+
+# rain data
+rain <- scan("http://robjhyndman.com/tsdldata/hurst/precip1.dat",skip=1) 
+rainseries <- ts(rain, start = c(1813))
+plot.ts(rainseries)
+
+#simple exponential smoothing
+rainseriesforecast <- HoltWinters(rainseries, beta = FALSE, gamma = FALSE)
+rainseriesforecast
+
+rainseriesforecast$fitted
+plot(rainseriesforecast)
+rainseriesforecast$SSE
+
+HoltWinters(rainseries, beta=FALSE, gamma = FALSE, l.start =23.56)
+
+rainseriesforecast2 <- forecast(rainseriesforecast, h=8)
+rainseriesforecast2
+plot(rainseriesforecast2)
+
+# correllogram
+acf(rainseriesforecast2$residuals, lag.max = 20, na.action = na.pass)
+
+# test
+Box.test(rainseriesforecast2$residuals, lag = 20, type = "Ljung-Box")
+plot.ts(rainseriesforecast2$residuals)
+
+plotForecastErrors <- function(forecasterrors)
+{
+  # make a histogram of the forecast errors:
+  mybinsize <- IQR(forecasterrors)/4 
+  mysd <- sd(forecasterrors)
+  mymin <- min(forecasterrors) - mysd*5 
+  mymax <- max(forecasterrors) + mysd*3
+  # generate normally distributed data with mean 0 and standard deviation mysd
+  mynorm <- rnorm(10000, mean=0, sd=mysd)
+  mymin2 <- min(mynorm)
+  mymax2 <- max(mynorm)
+  if (mymin2 < mymin) { mymin <- mymin2 }
+  if (mymax2 > mymax) { mymax <- mymax2 }
+  # make a red histogram of the forecast errors, with the normally distributed data overlaid:
+    mybins <- seq(mymin, mymax, mybinsize)
+  hist(forecasterrors, col="red", freq=FALSE, breaks=mybins)
+  # freq=FALSE ensures the area under the histogram = 1
+  # generate normally distributed data with mean 0 and standard deviation mysd
+  myhist <- hist(mynorm, plot=FALSE, breaks=mybins)
+  # plot the normal curve as a blue line on top of the histogram of forecast errors:
+  points(myhist$mids, myhist$density, type="l", col="blue", lwd=2)
+}
+
+rainseriesforecast2$residuals <- rainseriesforecast2$residuals[!is.na(rainseriesforecast2$residuals)]
+plotForecastErrors(rainseriesforecast2$residuals)
+
+skirts <- scan("http://robjhyndman.com/tsdldata/roberts/skirts.dat",skip=5) 
+skirtsseries <- ts(skirts, start = c(1866))
+plot.ts(skirtsseries)
+
+skirtsseriesforecasts <- HoltWinters(skirtsseries, gamma=FALSE)
+skirtsseriesforecasts
+
+skirtsseriesforecasts$SSE
+plot(skirtsseriesforecasts)
+
+#Holtwinters
+HoltWinters(skirtsseries, gamma=FALSE, l.start=608, b.start=9)
+
+skirtsseriesforecasts2 <- forecast(skirtsseriesforecasts, h=19)
+plot(skirtsseriesforecasts2)
+acf(na.omit(skirtsseriesforecasts2$residuals), lag.max=20)
+Box.test(na.omit(skirtsseriesforecasts2$residuals), lag=20, type="Ljung-Box")
+plot.ts(skirtsseriesforecasts2$residuals)
+skirtsseriesforecasts2$residuals <- skirtsseriesforecasts2$residuals[!is.na(skirtsseriesforecasts2$residuals)]
+plotForecastErrors(skirtsseriesforecasts2$residuals)
 
 
-# Calling the libraries
-library(datarium)
-library(RVAideMemoire)
-library(car)
-library(car)
- 
-# One-way ANOVA using Heart Attach Data in Datarium
-head(as.data.frame(heartattack))
-
-# Box plot to see if there are any outliers
-boxplot(cholesterol ~ drug, data = heartattack, names = c("A", "B", "C"),
-        xlab = "Types of Drug", ylab = "Cholesterol Level",
-        main = "Cholesterol level for each drug type")
-
-#Performing Shapiro-wilk test to check for normality
-byf.shapiro(cholesterol ~ drug, data = heartattack)
-# since the p-values are greater than 0.05, we accept the null hypothesis and can confirm that the data is nornally distributed
+logsouvenirtimeseries <- log(souvenirtimeseries)
+souvenirtimeseriesforecasts <- HoltWinters(logsouvenirtimeseries)
+souvenirtimeseriesforecasts
+souvenirtimeseriesforecasts$SSE
+plot(souvenirtimeseriesforecasts)
 
 
-# to check homogeniety of variances we use bartlett test
-bartlett.test(cholesterol ~ drug, data = heartattack)
-
-find("bartlett.test")
-
-#ANOVA Test
-oneway.test(cholesterol ~ drug, data = heartattack, var.equal = TRUE)
-# Since p-value is 0.07 (greater than 0.05), then we will accept the null hypothesis. ie there is no significant differnce between the three drugs
-
-?oneway.test
-
-# Two-way ANOVA
-summary(heartattack)
-byf.shapiro(cholesterol ~ drug*gender, data = heartattack)
-bartlett.test(cholesterol ~ interaction(drug, gender), data = heartattack)
-
-#the data is normal
-result_aov1 <- aov(cholesterol ~ drug * gender, data = heartattack)
-summary(result_aov1)
-
-#post-hoc analysis
-TukeyHSD(result_aov1, which = "gender")
-
-result_aov2 <- aov(cholesterol ~ drug + gender, data = heartattack)
-summary(result_aov2)
-
-#Two-way ANOVA (unbalanced data)
-summary(jobsatisfaction)
-head(jobsatisfaction)
-
-unb_anova <- aov(score ~ education_level * gender, data = jobsatisfaction)
-Anova(unb_anova, type = "III")
-
-?kruskal.test
-kruskal.test(cholesterol ~ drug, data = heartattack)
-
-
-#challenge
-plant <- PlantGrowth
-head(plant)
-summary(plant)
-
-byf.shapiro(weight ~ group, data = plant)
-oneway.test(weight ~ group, data = plant, var.equal = TRUE)
+souvenirtimeseriesforecasts2 <- forecast(souvenirtimeseriesforecasts,h=48)
+plot(souvenirtimeseriesforecasts2)
+acf(souvenirtimeseriesforecasts2$residuals, lag.max=20 , na.action = na.pass)
+Box.test(souvenirtimeseriesforecasts2$residuals, lag=20, type="Ljung-Box")
